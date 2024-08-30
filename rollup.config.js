c8a2ddf40b07418cf8d75196c0d87b38a09b56ea
Karbolin Ivan <karboliniv@gmail.com>
2024-08-30 04:40:58 +0300

    check

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import css from "rollup-plugin-import-css";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/app.js",
@@ -7,5 +8,13 @@ export default {
     dir: "dist",
     format: "iife",
   },
-  plugins: [css({ output: "bundle.css" }), nodeResolve()],
+  plugins: [
+    css({ output: "bundle.css" }),
+    nodeResolve(),
+    copy({
+      targets: [
+        { src: "index.html", dest: "dist" }, // Копирование index.html в dist
+      ],
+    }),
+  ],
 };
