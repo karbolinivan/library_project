2ab260cee3aea113bed529ebbcf676e90edf9258
karbolinivan <karbolinivan@users.noreply.github.com>
2024-08-30 13:31:25 +0000

    deploy: 6fbc0cc74d7102a5e6e5037a2f8f25cb764eeffb

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import css from "rollup-plugin-import-css";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+
+export default {
+  input: "src/app.js",
+  output: {
+    dir: "dist",
+    format: "iife",
+  },
+  plugins: [
+    css({ output: "bundle.css" }),
+    nodeResolve(),
+    copy({
+      targets: [
+        { src: "index.html", dest: "dist" }, // Копирование index.html в dist
+      ],
+    }),
+  ],
+};
